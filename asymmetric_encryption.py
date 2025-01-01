95f369dcec5d2b321835e7932a389c1cf78a9237
tetie12345 <floristabak@gmail.com>
2025-01-01 23:58:04 +0100

    licenced PrivMe under the GNU GPL

--- asymmetric_encryption.py
+++ asymmetric_encryption.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python
 
+# Copyright 2024, 2025 Floris Tabak
+#
+# This file is part of PrivMe
+# PrivMe is a free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public Licence as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# PrivMe is distributed in the hope taht it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have recieved a copy of the GNU General Public License
+# along with PrivMe. If not, see <https://www.gnu.org/licenses/>.
+
+
 from base64 import b64encode, b64decode
 import datetime
 from Cryptodome.PublicKey import RSA
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# Copyright 2024, 2025 Floris Tabak
+#
+# This file is part of PrivMe
+# PrivMe is a free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public Licence as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# PrivMe is distributed in the hope taht it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have recieved a copy of the GNU General Public License
+# along with PrivMe. If not, see <https://www.gnu.org/licenses/>.
+
 import socket, threading
 from time import sleep
 import asymmetric_encryption as ae
